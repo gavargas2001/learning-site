c4c662b615c709938f0245cfc6e50de801b34bc6
gavargas2001 <124389973+gavargas2001@users.noreply.github.com>
2023-11-10 10:59:15 -0600

    Commit from Stub AI (#3)
    Co-authored-by: gavargas2001 <bot@codestub.ai>

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -11,16 +11,33 @@ const CourseCard: React.FC<CourseCardProps> = ({ thumbnail, category, title, aut
   return (
     <div className="flex items-center justify-between space-x-4 p-6 bg-white rounded-lg shadow-md w-full hover:shadow-lg transition-shadow duration-300">
       <div className="flex items-center space-x-4">
-        <img className="w-1/4 h-32 object-cover rounded-lg" src={thumbnail} alt={title} />
+        <img className="w-1/2 h-48 object-cover rounded-lg" src={thumbnail} alt={title} />
         <div className="space-y-2">
           <p className="text-sm text-gray-500">{category}</p>
           <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
           <p className="text-sm text-gray-400">By {author}</p>
         </div>
       </div>
-      <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300">
-        Bookmark
-      </button>
+      <div className="flex items-center space-x-2">
+        <div className="relative">
+          <button className="p-2 rounded-full hover:bg-gray-200 transition-colors duration-300">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-500">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+            </svg>
+          </button>
+          <div className="absolute right-0 w-48 py-2 mt-2 bg-white rounded-md shadow-xl z-20">
+            <a href="#" className="block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
+              Add to Collection
+            </a>
+            <a href="#" className="block px-4 py-2 text-sm capitalize text-gray-700 hover:bg-blue-500 hover:text-white">
+              Share
+            </a>
+          </div>
+        </div>
+        <button className="bg-white text-blue-800 border-blue-800 border px-3 py-1 rounded text-sm hover:bg-blue-800 hover:text-white transition-colors duration-300">
+          Bookmark
+        </button>
+      </div>
     </div>
   );
 };
